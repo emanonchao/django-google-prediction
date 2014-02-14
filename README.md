1be36458159e3466cbc929a0537d7e2a779ab8e7
Gabriel Bianconi <bianconi.gabriel@gmail.com>
2014-02-13 22:58:43 -0800

    Add authentication with Google APIs and hosted models

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 ## Introduction
 
 *django-google-prediction* is a Django application that wraps the [Google Prediction API](https://developers.google.com/prediction/) to make it easier to build web applications with machine learning and data analysis functionality.
+
+## Instructions
+
+- Add private key to base directory
+
+- Add the following to settings.py:
+'''python
+GOOGLE_PREDICTION_PRIVATE_KEY = os.path.join(BASE_DIR, 'private-key.p12') # REPLACE WITH YOUR KEY
+GOOGLE_PREDICTION_PROJECT_EMAIL = '_________' # REPLACE WITH YOUR PROJECT EMAIL
+
+'''
--- google_prediction/models.py
+++ google_prediction/models.py
@@ -1,3 +1,48 @@
-from django.db import models
+import httplib2
+from django.conf import settings
+from oauth2client.client import SignedJwtAssertionCredentials
+from apiclient import discovery
 
-# Create your models here.
+class ModelManager:
+
+	def get_model(self, model):
+		if not hasattr(self, 'api'):
+			self.authorize()
+		return HostedModel(self.api, model)
+
+	@classmethod
+	def authorize(self):
+		# TODO Validate credentials
+		private_key = open(settings.GOOGLE_PREDICTION_PRIVATE_KEY).read()
+		http = httplib2.Http()
+
+		credentials = SignedJwtAssertionCredentials(
+			 	settings.GOOGLE_PREDICTION_PROJECT_EMAIL,
+			 	private_key,
+			 	[
+			      'https://www.googleapis.com/auth/devstorage.full_control',
+			      'https://www.googleapis.com/auth/devstorage.read_only',
+			      'https://www.googleapis.com/auth/devstorage.read_write',
+			      'https://www.googleapis.com/auth/prediction',
+			    ]
+			)
+
+		credentials.authorize(http)
+		ModelManager.api = discovery.build('prediction', 'v1.6', http=http)
+
+class HostedModel:
+	HOSTED_PROJECT_ID = 414649711441
+
+	models = ModelManager().get_model
+
+	def __init__(self, api, model):
+		self.api = api
+		self.model = model
+
+	def predict(self, inputData):
+		body = {'input': {'csvInstance': [inputData]}}
+		return ModelManager.api.hostedmodels().predict(
+			project=self.HOSTED_PROJECT_ID,
+			hostedModelName=self.model,
+			body=body
+		).execute()
